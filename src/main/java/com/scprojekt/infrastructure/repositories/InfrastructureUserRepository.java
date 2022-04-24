fa4de5d410faef6593756a6cf4417235c71b6df5
Claudius Pomorska <claudius.pomorska@scprojekt.com>
2022-04-24 23:11:51 +0200

    Prepared for Jakarta Rewrite

--- src/main/java/com/scprojekt/infrastructure/repositories/InfrastructureUserRepository.java
+++ src/main/java/com/scprojekt/infrastructure/repositories/InfrastructureUserRepository.java
@@ -1,14 +1,15 @@
 package com.scprojekt.infrastructure.repositories;
 
-import com.scprojekt.domain.interfaces.UserRepository;
-import com.scprojekt.domain.entities.User;
-import com.scprojekt.domain.entities.UserType;
+import com.scprojekt.domain.model.user.UserRepository;
+import com.scprojekt.domain.model.user.User;
+import com.scprojekt.domain.model.user.UserType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.transaction.Transactional;
+
 import java.util.List;
 import java.util.UUID;
 
